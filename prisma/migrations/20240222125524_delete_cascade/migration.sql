d53a5b88375523a2e7509f79f0d0207bf33e0eed
Ihor Pavliuk <akira.pavl@gmail.com>
2024-02-22 15:02:55 +0200

    + prisma module, cascade on delete

---
+++ prisma/migrations/20240222125524_delete_cascade/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "likes" DROP CONSTRAINT "likes_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "likes" DROP CONSTRAINT "likes_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_replyToId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_replyToId_fkey" FOREIGN KEY ("replyToId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
